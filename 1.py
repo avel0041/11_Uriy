020b78955795ed7308089e618448b24b9722cd73
YuriShept <yi.sheptv@yandex.ru>
2025-11-26 21:16:34 +0300

    26_8

--- 1.py
+++ 1.py
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-f = open('26_5.txt').readlines()[1:]
-n = f[0]
-a = sorted([int(x) for x in f[1:]])[::-1]
-k = 0
-r = 0
-
-ggfgf
-# 1
-while len(a)>1:
-    if a[0] - a[1] >= 9:
-        k+=1
-        r = a[1]
-        a.remove(a[0])
-        a.remove(a[1])
-    else:
-        a.remove(a[1])
-print(k, r)
-=======
-f = open('26_8.txt').readlines()
+f = open('C:/Users/Юрий/Desktop/sadaf/11_Uriy/26_8.txt').readlines()
 sd = []
 nesd = []
 for g in f[1:]:
@@ -27,8 +8,7 @@ for g in f[1:]:
         a = [-sr] + [a[0]]
         sd.append(a)
     if 2 in a[1:]:
-        sr = (sum(a[1:]))/4
-        a = [-sr] + a
+        a = sorted(a[1:])[::-1] + [a[0]]
         nesd.append(a)
 
 n = int(f[0])//4
@@ -36,7 +16,7 @@ sd = sorted(sd)
 print(sd[n-1])
 
 nesd = sorted(nesd)
-for i in nesd:
-    if nesd.count(2)==3:
-        
->>>>>>> 85af6feaeaa00eebd2696b7efeed1c2f22153125
+for i in range(len(nesd)):
+    if nesd[i].count(2)>=3:
+        print(nesd[i][-1])
+        break
